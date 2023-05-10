96f0bc07c904f40b1e9723b966f7537580f06790
= <=>
2023-05-10 14:12:16 +0530

    create-app

---
+++ Crafticon/ecommerce-frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header>
+        <a href="/">Crafticon</a>
+      </header>
+      <main>
+        <h1>list project</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
